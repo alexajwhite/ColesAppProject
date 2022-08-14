7c85e63cac31b445479948fd8f274251f3424240
Alex <alexajwhite@outlook.com>
2022-08-14 12:53:00 +1000

    Initial Commit

---
+++ ColesApp/ColesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColesAppApp.swift
+//  ColesApp
+//
+//  Created by Alexander White on 14/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
